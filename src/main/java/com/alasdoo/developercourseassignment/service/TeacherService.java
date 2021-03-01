2311da35e7d44a49897b6c3d5c1e900a34fc04fe
markomarko <markogajicgaja@gmail.com>
2021-03-01 14:51:28 +0100

    a

---
+++ src/main/java/com/alasdoo/developercourseassignment/service/TeacherService.java
@@ -0,0 +1,12 @@
+package com.alasdoo.developercourseassignment.service;
+
+import com.alasdoo.developercourseassignment.dto.StudentDTO;
+import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+
+public interface TeacherService extends CrudService<TeacherDTO> {
+	
+	TeacherDTO findByTeacherEmail(String email);
+
+	TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surename);
+
+}
